4731a7e0b1ddca23142aeccb875524f0c46b7260
Omar OUGHZAL <omar.oughzal@gmail.com>
2023-11-23 15:55:35 +0100

    Initial Commit

--- app/src/main/java/com/omarcomputer/quiz/MainActivity.java
+++ app/src/main/java/com/omarcomputer/quiz/MainActivity.java
@@ -19,8 +19,9 @@ public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
     QuestionViewModel questionViewModel;
+    TextView[] answers;
 
-    Question question;
+    Question question = new Question();
     int currentIndex;
     int scoreCorrect=0;
     int scoreIncorrect=0;
@@ -30,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+        answers = new TextView[] {binding.answer1, binding.answer2, binding.answer3, binding.answer4};
         questionViewModel = new ViewModelProvider(this).get(QuestionViewModel.class);
         questionViewModel.currentIndex.observe(this, index -> {
             currentIndex = questionViewModel.currentIndex.getValue();
@@ -42,12 +43,32 @@ public class MainActivity extends AppCompatActivity {
             binding.answer2.setText(question.getReponse2());
             binding.answer3.setText(question.getReponse3());
             binding.answer4.setText(question.getReponse4());
-            setEvents();
+
 
         });
         questionViewModel.getQuestion();
         TextView[] answers = {binding.answer1, binding.answer2, binding.answer3, binding.answer4};
+        for (int i = 0; i < 4; i++) {
+            TextView answer = answers[i];
+            Log.i("Quizzzz",""+question.correct);
+            int finalI = i;
+            answer.setOnClickListener(view -> {
+                lockAnswers();
+                if (finalI == question.correct) {
+                    scoreCorrect++;
+                    answer.setBackgroundResource(R.drawable.correct_answer);
+                } else {
+                    scoreIncorrect++;
+                    answer.setBackgroundResource(R.drawable.incorrect_answer);
+                }
+                if(currentIndex < questionViewModel.quetionList.getValue().size() -1){
+                    questionViewModel.currentIndex.setValue(currentIndex +1);
+                    resetAnswers();
+                }
 
+            });
+
+        }
 
 
     }
@@ -67,40 +88,14 @@ public class MainActivity extends AppCompatActivity {
     void resetAnswers (){
         binding.scoreCorrect.setText(""+ scoreCorrect);
         binding.scoreIncorrect.setText(""+ scoreIncorrect);
-        TextView[] answers = {binding.answer1, binding.answer2, binding.answer3, binding.answer4};
-
-        for (int i = 0; i < 4; i++) {
+         for (int i = 0; i < 4; i++) {
             TextView answer = answers[i];
             answer.setEnabled(true);
             answer.setBackgroundResource(R.drawable.rectangle2);
         }
     }
     void setEvents(){
-        TextView[] answers = {binding.answer1, binding.answer2, binding.answer3, binding.answer4};
-        for (int i = 0; i < 4; i++) {
-            TextView answer = answers[i];
 
-            Log.i("Quizzzz",""+question.correct);
-            int finalI = i;
-            answer.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    lockAnswers();
-                    if (finalI == question.correct) {
-                        scoreCorrect++;
-                        answer.setBackgroundResource(R.drawable.correct_answer);
-                    } else {
-                        scoreIncorrect++;
-                        answer.setBackgroundResource(R.drawable.incorrect_answer);
-                    }
-                    if(currentIndex < questionViewModel.quetionList.getValue().size() -1){
-                        questionViewModel.currentIndex.setValue(currentIndex +1);
-                        resetAnswers();
-                    }
 
-                }
-            });
-
-        }
     }
 }
